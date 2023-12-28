e3ec62f236e1aa868ab00b7e7fb8d0b08d52959f
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-12-29 00:23:32 +0530

    adding mime type

--- HospitalManagement/HospitalManagement/settings.py
+++ HospitalManagement/HospitalManagement/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
+
+mimetypes.add_type("text/css",'.css',True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
