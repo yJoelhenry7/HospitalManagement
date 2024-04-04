9b47d2c4bfaa294e3e2be64657c28a09284dc48c
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2024-04-04 09:53:58 +0530

    changing settings

--- HospitalManagement/HospitalManagement/settings.py
+++ HospitalManagement/HospitalManagement/settings.py
@@ -15,7 +15,7 @@ import os
 import mimetypes
 
 mimetypes.add_type("text/css",'.css',True)
-mimetypes.add_type("text/js",'.js',True)
+mimetypes.add_type("text/javascript", '.js', True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-o3%+d5dw+exj$y%16d)xbm680rsm#h3nd@8y#(1$8@8qn8bff0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
@@ -122,9 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
